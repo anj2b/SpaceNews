80138e8abd54946a4c574b2d96c4b7e4d89b02df
anjaneya2b <anjaneya2b@gmail.com>
2021-08-20 15:52:00 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// React Native Counter Example using Hooks!
 
-export default function App() {
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getArticles = async () => {
+     try {
+      const response = await fetch('https://api.spaceflightnewsapi.net/v3/articles');
+      const json = await response.json();
+      console.log(json);
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+            <Text>{item.title}</Text>
+            <Image
+              style={{width: 100, height: 100}}
+              source={{
+              uri: item.imageUrl,
+              }}
+            />
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
+// React Native Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 1,
+    borderRadius: 10,
+    flexDirection: 'col',
+    alignItems: 'center',
   },
 });
+
+export default App;
